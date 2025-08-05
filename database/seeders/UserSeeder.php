afac359829d541278844d6727beb06c02447e3e7
Muhdani Boyrend Erlan Azhari <muhdaniboyrendi@gmail.com>
2025-08-05 10:50:30 +0700

    feat: Remove obsolete deployment scripts and Docker configuration files; streamline project structure

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
             'name' => 'Muhdani Boyrendi Erlan Azhari',
             'email' => 'erlanazrdev@gmail.com',
             'phone' => '082220633024',
-            // 'password' => 'AkuLali87',
+            // 'password' => '12345678',
             'role' => 'admin',
         ]);
     }
